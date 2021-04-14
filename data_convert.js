d2186c05c2e83997e70ec55326fcd7adc12c69be
rudwo8134 <rudwo8134@gmail.com>
2021-04-14 09:51:13 -0400

    remove

--- data_convert.js
+++ data_convert.js
@@ -185,10 +185,3 @@ $(document).ready(function(){
    
   });
 
-
-  jQuery(document).ready(function($) {
-    $(".scroll").click(function(event){            
-    event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
-    });
-    });
