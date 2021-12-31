e7c0e9b2fd96ea7fca8a9fc908546936d51e46d9
Eric Shin <92757570+eshin-at-speer@users.noreply.github.com>
2021-12-31 04:02:06 -0500

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Autocomplete,
+  LoadScript,
+} from '@react-google-maps/api';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import  { regionData } from './Regiondata.jsx';
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+const containerStyle = {
+  width: '100vw',
+  height: '100vh',
+};
+
+const center = {
+  lat: 43.65107,
+  lng: -79.347015,
+};
 
 function App() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
+  });
+  const [map, setMap] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [canadaData, setCanadaData] = useState(null);
+  const [Citydata, setCityData] = useState(null);
+  const getOntarioData = async () => {
+    const fetchdata = await axios.get(
+      'https://api.opencovid.ca/summary?loc=ON&date=30-12-2021&version=true'
+    );
+    setCanadaData(fetchdata?.data);
+  };
+
+  const getAllCityData = async () => {
+    
+  };
+  console.log(Citydata);
+  console.log(canadaData);
+
+  useEffect(() => {
+    setLoading(true);
+    getOntarioData();
+    getAllCityData();
+    setLoading(false);
+  }, []);
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  const defaultMapOptions = {
+    fullscreenControl: false,
+    streetViewControl: false,
+    mapTypeControl: false,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      {isLoaded && (
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+          onLoad={(map) => onLoad(map)}
+          onUnmount={() => onUnmount(map)}
+          options={defaultMapOptions}
+        />
+      )}
+    </Wrapper>
   );
 }
 
