6e9f1ce1d04b0b385e8964315a0ca54549c044ec
Eric Shin <92757570+eshin-at-speer@users.noreply.github.com>
2022-01-01 10:34:44 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -6,10 +6,30 @@ import {
   useJsApiLoader,
   Autocomplete,
   LoadScript,
+  Polyline,
+  Polygon,
+  Circle,
 } from '@react-google-maps/api';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import  { regionData } from './Regiondata.jsx';
+import { toronto } from './Polygon/toroto';
+import { Peel } from './Polygon/peelregion';
+import { Durham } from './Polygon/durhamregion';
+import { Brant } from './Polygon/Brant';
+import { Algoma } from './Polygon/Algoma';
+import { Chathamkent } from './Polygon/chathamKent';
+import { Northern } from './Polygon/Northern';
+import { Greybruce } from './Polygon/Greybruce';
+import { Haldimand } from './Polygon/Haldimand';
+import { Haliburton } from './Polygon/Haliburton';
+import { Holton } from './Polygon/Holton';
+import { Hamilton } from './Polygon/Hamilton';
+import { PrinceEdward } from './Polygon/princeedward';
+import { Huron } from './Polygon/Huron';
+import { Kingston } from './Polygon/Kingston';
+import { Lambton } from './Polygon/lambton';
+import { Leeds } from './Polygon/Leeds';
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -22,8 +42,8 @@ const containerStyle = {
 };
 
 const center = {
-  lat: 43.65107,
-  lng: -79.347015,
+  lat: -79.347015,
+  lng: 43.65107,
 };
 
 function App() {
@@ -71,17 +91,119 @@ function App() {
     mapTypeControl: false,
   };
 
+  const options = {
+    fillColor: 'lightblue',
+    fillOpacity: 0.6,
+    strokeColor: 'none',
+    strokeOpacity: 0,
+    strokeWeight: 2,
+    clickable: false,
+    draggable: false,
+    editable: false,
+    geodesic: false,
+    zIndex: 1,
+  };
+
   return (
     <Wrapper>
       {isLoaded && (
         <GoogleMap
           mapContainerStyle={containerStyle}
-          center={center}
+          center={{
+            lat: 43.65107,
+            lng: -79.347015,
+          }}
           zoom={10}
           onLoad={(map) => onLoad(map)}
           onUnmount={() => onUnmount(map)}
           options={defaultMapOptions}
-        />
+        >
+          <Polygon
+            path={Peel}
+            onLoad={(e) => console.log(e)}
+            options={options}
+          />
+          <Polygon
+            path={toronto}
+            onLoad={(e) => console.log(e)}
+            options={options}
+          />
+          <Polygon
+            path={Durham}
+            onLoad={(e) => console.log(e)}
+            option={options}
+          />
+          <Polygon
+            path={Algoma}
+            onLoad={(e) => console.log(e)}
+            option={options}
+          />
+          <Polygon
+            path={Brant}
+            onLoad={(e) => console.log(e)}
+            option={options}
+          />
+          <Polygon
+            path={Chathamkent}
+            onLoad={(e) => console.log(e)}
+            option={options}
+          />
+          <Polygon
+            path={Northern}
+            onLoad={(e) => console.log(e)}
+            option={options}
+          />
+          <Polygon
+            path={Greybruce}
+            onLoad={(e) => console.log(e)}
+            option={options}
+          />
+          <Polygon
+            path={Haldimand}
+            onLoad={(e) => console.log(e)}
+            option={options}
+          />
+          <Polygon
+            path={Haliburton}
+            onLoad={(e) => console.log(e)}
+            option={options}
+          />
+          <Polygon
+            path={Holton}
+            onLoad={(e) => console.log(e)}
+            option={options}
+          />
+          <Polygon
+            path={Hamilton}
+            onLoad={(e) => console.log(e)}
+            option={options}
+          />
+          <Polygon
+            path={PrinceEdward}
+            onLoad={(e) => console.log(e)}
+            options={options}
+          />
+          <Polygon
+            path={Huron}
+            onLoad={(e) => console.log(e)}
+            option={options}
+          />
+          <Polygon
+            path={Kingston}
+            onLoad={(e) => console.log(e)}
+            option={options}
+          />
+          <Polygon
+            path={Lambton}
+            onLoad={(e) => console.log(e)}
+            option={options}
+          />
+          <Polygon
+            path={Leeds}
+            onLoad={(e) => console.log(e)}
+            option={options}
+          />
+        </GoogleMap>
       )}
     </Wrapper>
   );
