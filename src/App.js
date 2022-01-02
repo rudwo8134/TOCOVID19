cef5ee5c54b99ac1e76fbffddd65d3f97b5585b3
Eric Shin <92757570+eshin-at-speer@users.noreply.github.com>
2022-01-01 23:15:10 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,39 +1,122 @@
-import logo from './logo.svg';
 import './App.css';
 import styled from 'styled-components';
 import {
   GoogleMap,
   useJsApiLoader,
-  Autocomplete,
-  LoadScript,
-  Polyline,
   Polygon,
-  Circle,
+  OverlayView,
 } from '@react-google-maps/api';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
-import  { regionData } from './Regiondata.jsx';
-import { toronto } from './Polygon/toroto';
-import { Peel } from './Polygon/peelregion';
-import { Durham } from './Polygon/durhamregion';
-import { Brant } from './Polygon/Brant';
-import { Algoma } from './Polygon/Algoma';
-import { Chathamkent } from './Polygon/chathamKent';
-import { Northern } from './Polygon/Northern';
-import { Greybruce } from './Polygon/Greybruce';
-import { Haldimand } from './Polygon/Haldimand';
-import { Haliburton } from './Polygon/Haliburton';
-import { Holton } from './Polygon/Holton';
-import { Hamilton } from './Polygon/Hamilton';
-import { PrinceEdward } from './Polygon/princeedward';
-import { Huron } from './Polygon/Huron';
-import { Kingston } from './Polygon/Kingston';
-import { Lambton } from './Polygon/lambton';
-import { Leeds } from './Polygon/Leeds';
+import { regionData } from './Regiondata.jsx';
+import moment from 'moment';
+import Image5 from './img/VeryDanger.png';
+import Image4 from './img/danger.png';
+import Image3 from './img/medium.png';
+import Image2 from './img/low.png';
+import Image1 from './img/safe.png';
+import { ClipLoader } from 'react-spinners';
 
 const Wrapper = styled.div`
   width: 100vw;
   height: 100vh;
+  .infobox {
+    width: 30vw;
+    height: 25vh;
+    background-color: white;
+    position: absolute;
+    z-index: 300;
+    top: 1rem;
+    left: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #444;
+    border-radius: 16px;
+    color: white;
+    @media screen and (max-width: 476px) {
+      width: 70vw;
+      height: 20vh;
+    }
+    h1 {
+      font-size: 1.5rem;
+      text-transform: uppercase;
+      @media screen and (max-width: 476px) {
+        font-size: 1rem;
+      }
+    }
+    .boxcontainer {
+      width: 90%;
+      padding: 0 1rem;
+      display: flex;
+      flex-direction: column;
+      @media screen and (max-width: 476px) {
+        padding: 0 0.3rem;
+      }
+      .update {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        @media screen and (max-width: 476px) {
+          margin-top: 0.4rem;
+          line-height: 0.6rem;
+        }
+        h4 {
+          flex: 1;
+          text-align: center;
+          font-size: 0.6rem;
+          margin: 0;
+          @media screen and (max-width: 476px) {
+            font-size: 0.4rem;
+          }
+        }
+      }
+      span {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 1rem;
+        @media screen and (max-width: 476px) {
+          margin-bottom: 0.4rem;
+          font-size: 0.9rem;
+          .textc {
+            font-size: 0.47rem;
+          }
+        }
+        @media screen and (max-width: 375px) {
+          margin-bottom: 0.4rem;
+          font-size: 0.9rem;
+          .textc {
+            font-size: 0.41rem;
+          }
+        }
+        @media screen and (max-width: 320px) {
+          margin-bottom: 0.4rem;
+          font-size: 0.7rem;
+          .textc {
+            font-size: 0.35rem;
+          }
+        }
+        :last-child {
+          margin-bottom: 0;
+        }
+        .text {
+          flex: 1;
+          text-align: center;
+        }
+        .textb {
+          flex: 1;
+          text-align: center;
+          font-weight: 900;
+        }
+      }
+    }
+  }
 `;
 
 const containerStyle = {
@@ -41,168 +124,268 @@ const containerStyle = {
   height: '100vh',
 };
 
-const center = {
-  lat: -79.347015,
-  lng: 43.65107,
-};
+const Loadingcontainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
+  font-weight: 900;
+  text-transform: uppercase;
+`;
 
 function App() {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
   });
-  const [map, setMap] = useState(null);
+  const mapRef = useRef(null);
+  const [position, setPosition] = useState({
+    lng: -79.347015,
+    lat: 43.65107,
+  });
   const [loading, setLoading] = useState(false);
   const [canadaData, setCanadaData] = useState(null);
   const [Citydata, setCityData] = useState(null);
   const getOntarioData = async () => {
     const fetchdata = await axios.get(
-      'https://api.opencovid.ca/summary?loc=ON&date=30-12-2021&version=true'
+      'https://api.opencovid.ca/summary?loc=ON&version=true'
     );
     setCanadaData(fetchdata?.data);
+    getAllCityData();
   };
 
   const getAllCityData = async () => {
-    
+    const Fetchdata = regionData.map(async (data) => {
+      const todayData = await moment(canadaData?.version).format('DD-MM-YYYY');
+      const data2 = await axios.get(
+        `https://api.opencovid.ca/summary?loc=${data?.code}&date=${todayData}&version=true`
+      );
+      const Finaldata = data2?.data?.summary[0];
+      return { ...data, ...Finaldata };
+    });
+    const data3 = await Promise.all(Fetchdata);
+    setCityData(data3);
   };
-  console.log(Citydata);
-  console.log(canadaData);
 
   useEffect(() => {
     setLoading(true);
     getOntarioData();
-    getAllCityData();
     setLoading(false);
   }, []);
 
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map);
-  }, []);
+  function handleLoad(map) {
+    mapRef.current = map;
+  }
 
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null);
-  }, []);
+  function handleCenter() {
+    if (!mapRef.current) return;
+
+    const newPos = mapRef.current.getCenter().toJSON();
+    setPosition(newPos);
+  }
+
+  const getPixelPositionOffset = (offsetWidth, offsetHeight, labelAnchor) => {
+    return {
+      x: offsetWidth + labelAnchor.x,
+      y: offsetHeight + labelAnchor.y,
+    };
+  };
 
   const defaultMapOptions = {
     fullscreenControl: false,
     streetViewControl: false,
     mapTypeControl: false,
+    minZoom: 8,
+    maxZoom: 11,
   };
 
-  const options = {
-    fillColor: 'lightblue',
-    fillOpacity: 0.6,
-    strokeColor: 'none',
-    strokeOpacity: 0,
-    strokeWeight: 2,
-    clickable: false,
-    draggable: false,
-    editable: false,
-    geodesic: false,
-    zIndex: 1,
-  };
-
+  if (Citydata === null || loading) {
+    return (
+      <Loadingcontainer>
+        Wait for a moment!
+        <ClipLoader loading={true} size={150}></ClipLoader>
+      </Loadingcontainer>
+    );
+  }
   return (
     <Wrapper>
+      <div className="infobox">
+        <h1>Ontario CovidMap</h1>
+        <div className="boxcontainer">
+          <span>
+            <div className="text textc">State/City</div>
+            <div className="text textc" style={{ color: 'red' }}>
+              Active
+            </div>
+            <div className="text textc" style={{ color: 'green' }}>
+              Recover
+            </div>
+            <div className="text textc" style={{ color: '#ff5b00' }}>
+              New Case(Today)
+            </div>
+          </span>
+          <span>
+            <div className="textb">Ontario</div>
+            <div className="text">
+              {canadaData?.summary[0]?.active_cases
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            </div>
+            <div className="text">
+              {canadaData?.summary[0]?.recovered
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            </div>
+            <div className="text">
+              {canadaData?.summary[0]?.cases
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            </div>
+          </span>
+          <span>
+            <div className="text textc"> </div>
+            <div className="text textc" style={{ color: 'red' }}>
+              Daeths (Today)
+            </div>
+            <div className="text textc" style={{ color: '#ffc302' }}>
+              Testing (Today)
+            </div>
+            <div className="text textc" style={{ color: '#22bb33' }}>
+              Vaccine (Today)
+            </div>
+          </span>
+          <span>
+            <div className="textb"></div>
+            <div className="text">
+              {canadaData?.summary[0]?.deaths
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            </div>
+            <div className="text">
+              {canadaData?.summary[0]?.testing
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            </div>
+            <div className="text">
+              {canadaData?.summary[0]?.dvaccine
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            </div>
+          </span>
+          {canadaData && (
+            <span className="update">
+              <h4>
+                Updated on{' '}
+                {moment(canadaData?.version).format('HH:mm MM, DD, YYYY')}
+                <br />
+                any questions? rudwo8134@gmail.com
+              </h4>
+            </span>
+          )}
+        </div>
+      </div>
       {isLoaded && (
         <GoogleMap
           mapContainerStyle={containerStyle}
-          center={{
-            lat: 43.65107,
-            lng: -79.347015,
-          }}
+          center={position}
           zoom={10}
-          onLoad={(map) => onLoad(map)}
-          onUnmount={() => onUnmount(map)}
+          onLoad={handleLoad}
+          onDragEnd={handleCenter}
           options={defaultMapOptions}
         >
-          <Polygon
-            path={Peel}
-            onLoad={(e) => console.log(e)}
-            options={options}
-          />
-          <Polygon
-            path={toronto}
-            onLoad={(e) => console.log(e)}
-            options={options}
-          />
-          <Polygon
-            path={Durham}
-            onLoad={(e) => console.log(e)}
-            option={options}
-          />
-          <Polygon
-            path={Algoma}
-            onLoad={(e) => console.log(e)}
-            option={options}
-          />
-          <Polygon
-            path={Brant}
-            onLoad={(e) => console.log(e)}
-            option={options}
-          />
-          <Polygon
-            path={Chathamkent}
-            onLoad={(e) => console.log(e)}
-            option={options}
-          />
-          <Polygon
-            path={Northern}
-            onLoad={(e) => console.log(e)}
-            option={options}
-          />
-          <Polygon
-            path={Greybruce}
-            onLoad={(e) => console.log(e)}
-            option={options}
-          />
-          <Polygon
-            path={Haldimand}
-            onLoad={(e) => console.log(e)}
-            option={options}
-          />
-          <Polygon
-            path={Haliburton}
-            onLoad={(e) => console.log(e)}
-            option={options}
-          />
-          <Polygon
-            path={Holton}
-            onLoad={(e) => console.log(e)}
-            option={options}
-          />
-          <Polygon
-            path={Hamilton}
-            onLoad={(e) => console.log(e)}
-            option={options}
-          />
-          <Polygon
-            path={PrinceEdward}
-            onLoad={(e) => console.log(e)}
-            options={options}
-          />
-          <Polygon
-            path={Huron}
-            onLoad={(e) => console.log(e)}
-            option={options}
-          />
-          <Polygon
-            path={Kingston}
-            onLoad={(e) => console.log(e)}
-            option={options}
-          />
-          <Polygon
-            path={Lambton}
-            onLoad={(e) => console.log(e)}
-            option={options}
-          />
-          <Polygon
-            path={Leeds}
-            onLoad={(e) => console.log(e)}
-            option={options}
-          />
+          {Citydata &&
+            Citydata.map((data) => {
+              const Colorcode =
+                data?.cases > 1500
+                  ? '#bb2124'
+                  : data?.cases > 1000
+                  ? '#ff5b00'
+                  : data?.cases > 500
+                  ? '#ff8f00'
+                  : data?.cases > 250
+                  ? '#ffc302'
+                  : data?.cases > 150
+                  ? '#fff600'
+                  : '#22bb33';
+              const Imageicon =
+                data?.cases > 1500
+                  ? Image5
+                  : data?.cases > 1000
+                  ? Image4
+                  : data?.cases > 500
+                  ? Image3
+                  : data?.cases > 250
+                  ? Image2
+                  : data?.cases > 150
+                  ? Image2
+                  : Image1;
+
+              return (
+                <>
+                  <Polygon
+                    key={data.id}
+                    path={data?.Polygon}
+                    options={{
+                      fillColor: Colorcode,
+                      fillOpacity: 0.6,
+                      strokeColor: 'black',
+                      strokeOpacity: 0.5,
+                      strokeWeight: 0.5,
+                      clickable: false,
+                      draggable: false,
+                      editable: false,
+                      geodesic: false,
+                      zIndex: `${data.cases * 1}`,
+                    }}
+                  />
+                  <OverlayView
+                    key="mwl"
+                    position={data?.coordinate}
+                    mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+                    getPixelPositionOffset={(x, y) =>
+                      getPixelPositionOffset(x, y, { x: 0, y: -100 })
+                    }
+                  >
+                    <div
+                      style={{
+                        background: `#203254`,
+                        padding: `7px 12px`,
+                        fontSize: '11px',
+                        color: `white`,
+                        borderRadius: '4px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        zIndex: `${data.cases * 1}`,
+                        border: `1px solid ${Colorcode}`,
+                      }}
+                    >
+                      <img
+                        style={{ width: '20px', height: '20px' }}
+                        src={Imageicon}
+                        alt="lol"
+                      />
+                      {data?.Region}
+                      <br />
+
+                      <span
+                        style={{
+                          color: `white`,
+                          fontWeight: 900,
+                          marginLeft: '3px',
+                        }}
+                      >
+                        Today: {data?.cases}
+                      </span>
+                    </div>
+                  </OverlayView>
+                </>
+              );
+            })}
         </GoogleMap>
       )}
     </Wrapper>
--- src/Regiondata.jsx
+++ src/Regiondata.jsx
@@ -1,206 +1,275 @@
+import { Algoma } from "./Polygon/Algoma";
+import { Brant } from "./Polygon/Brant";
+import { Chathamkent } from "./Polygon/chathamKent";
+import { Durham } from "./Polygon/durhamregion";
+import { Greybruce } from "./Polygon/Greybruce";
+import { Guelph } from "./Polygon/Guelph";
+import { Haldimand } from "./Polygon/Haldimand";
+import { Haliburton } from "./Polygon/Haliburton";
+import { Hamilton } from "./Polygon/Hamilton";
+import { Holton } from "./Polygon/Holton";
+import { Huron } from "./Polygon/Huron";
+import { Kingston } from "./Polygon/Kingston";
+import { Lambton } from "./Polygon/lambton";
+import { Leeds } from "./Polygon/Leeds";
+import { London } from "./Polygon/London";
+import { Niagara } from "./Polygon/Niagara";
+import { Northbay } from "./Polygon/Northbay";
+import { Northern } from "./Polygon/Northern";
+import { NorthWestern } from "./Polygon/NorthWestern";
+import { Ottawa } from "./Polygon/Ottawa";
+import { Peel } from "./Polygon/peelregion";
+import { Peterborough } from "./Polygon/Peterborugh";
+import { Porcupine } from "./Polygon/Porcupine";
+import { PrinceEdward } from "./Polygon/princeedward";
+import { Renfrew } from "./Polygon/Renfrew";
+import { Simcoe } from "./Polygon/Simcoe";
+import { Southwestern } from "./Polygon/Southwestern";
+import { Sudbury } from "./Polygon/Sudbury";
+import { Thunderbay } from "./Polygon/Thunderbay";
+import { Timiskaming } from "./Polygon/Timiskaming";
+import { toronto } from "./Polygon/toroto";
+import { Waterloo } from "./Polygon/Waterloo";
+import { Windsor } from "./Polygon/Windsor";
+import { York } from "./Polygon/York";
+
 export const regionData = [
   {
     id: 1,
     Region: 'Algoma',
     code: 3526,
-    coordinate: { lat: 48.55495, lng: -84.700104 },
+    coordinate: { lat: 46.532211, lng: -84.3172187 },
+    Polygon: Algoma,
   },
   {
     id: 2,
     Region: 'Brant',
     code: 3527,
-    coordinate: { lat: 43.116669, lng: -80.366669 },
+    coordinate: { lat: 43.151859, lng: -80.2766627 },
+    Polygon: Brant,
   },
   {
     id: 3,
     Region: 'Chatham-Kent',
     code: 3540,
-    coordinate: { lat: 42.404804, lng: -82.19104 },
+    coordinate: { lat: 42.4038131, lng: -82.2107812 },
+    Polygon: Chathamkent,
   },
   {
     id: 4,
     Region: 'Durham',
     code: 3530,
-    coordinate: { lat: 44.0183187, lng: -79.3864454 },
+    coordinate: { lat: 43.8848583, lng: -78.9124946 },
+    Polygon: Durham,
   },
   {
     id: 5,
     Region: 'North Eastern',
     code: 3558,
     coordinate: { lat: 47.3989546, lng: -81.9646191 },
+    Polygon: Northern,
   },
   {
     id: 6,
     Region: 'Grey Bruce',
     code: 3533,
     coordinate: { lat: 44.4594867, lng: -81.2451061 },
+    Polygon: Greybruce,
   },
   {
     id: 7,
     Region: 'Haldimand-Norfolk',
     code: 3534,
     coordinate: { lat: 42.7306055, lng: -80.5868254 },
+    Polygon: Haldimand,
   },
   {
     id: 8,
     Region: 'Haliburton Kawartha Pineridge',
     code: 3535,
     coordinate: { lat: 44.9040597, lng: -78.7019828 },
+    Polygon: Haliburton,
   },
   {
     id: 9,
     Region: 'Halton',
     code: 3536,
-    coordinate: { lat: 43.5103274, lng: -80.0792508 },
+    coordinate: { lat: 43.5103274, lng: -80.0092508 },
+    Polygon: Holton,
   },
   {
     id: 10,
     Region: 'Hamilton',
     code: 3537,
-    coordinate: { lat: 43.2609686, lng: -80.2133275 },
+    coordinate: { lat: 43.2609686, lng: -80.1133275 },
+    Polygon: Hamilton,
   },
   {
     id: 11,
     Region: 'Hastings Prince Edward',
     code: 3538,
     coordinate: { lat: 44.0091151, lng: -77.6703606 },
+    Polygon: PrinceEdward,
   },
   {
     id: 12,
     Region: 'Huron Perth',
     code: 3539,
     coordinate: { lat: 43.4611503, lng: -81.3251833 },
+    Polygon: Huron,
   },
   {
     id: 13,
     Region: 'Kingston Frontenac Lennox & Addington',
     code: 3541,
-    coordinate: { lat: 44.2460445, lng: -76.5019707 },
+    coordinate: { lat: 44.6460445, lng: -77.0019707 },
+    Polygon: Kingston,
   },
   {
     id: 14,
     Region: 'Lambton',
     code: 3542,
     coordinate: { lat: 43.0015634, lng: -82.2942654 },
+    Polygon: Lambton,
   },
   {
     id: 15,
     Region: 'Leeds Grenville and Lanark',
     code: 3543,
     coordinate: { lat: 44.7331911, lng: -76.0094092 },
+    Polygon: Leeds,
   },
   {
     id: 16,
     Region: 'Middlesex-London',
     code: 3544,
     coordinate: { lat: 42.9725595, lng: -81.2804561 },
+    Polygon: London,
   },
   {
     id: 17,
     Region: 'Niagara',
     code: 3546,
     coordinate: { lat: 43.0715158, lng: -79.3368854 },
+    Polygon: Niagara,
   },
   {
     id: 18,
     Region: 'North Bay Parry Sound',
     code: 3547,
     coordinate: { lat: 45.3599835, lng: -80.0481362 },
+    Polygon: Northbay,
   },
   {
     id: 19,
     Region: 'Northwestern',
     code: 3549,
     coordinate: { lat: 50.4934978, lng: -90.9638383 },
+    Polygon: NorthWestern,
   },
   {
     id: 20,
     Region: 'Ottawa',
     code: 3551,
     coordinate: { lat: 45.2502975, lng: -76.0804344 },
+    Polygon: Ottawa,
   },
   {
     id: 21,
     Region: 'Peel',
     code: 3553,
-    coordinate: { lat: 43.7036977, lng: -79.9415781 },
+    coordinate: { lat: 43.68187157401338, lng: -79.8157090677861 },
+    Polygon: Peel,
   },
   {
     id: 22,
     Region: 'Peterborough',
     code: 3555,
     coordinate: { lat: 44.3152959, lng: -78.3956135 },
+    Polygon: Peterborough,
   },
   {
     id: 23,
     Region: 'Porcupine',
     code: 3556,
     coordinate: { lat: 48.4937888, lng: -81.2011818 },
+    Polygon: Porcupine,
   },
   {
     id: 24,
     Region: 'Renfrew',
     code: 3557,
     coordinate: { lat: 45.47491, lng: -76.69125598 },
+    Polygon: Renfrew,
   },
   {
     id: 25,
     Region: 'Simcoe Muskoka',
     code: 3560,
-    coordinate: { lat: 44.8577772, lng: -79.8501071 },
+    coordinate: { lat: 44.7149664, lng: -79.6649377 },
+    Polygon: Simcoe,
   },
   {
     id: 26,
     Region: 'Southwestern',
     code: 3575,
     coordinate: { lat: 42.0719666, lng: -82.7104213 },
+    Polygon: Southwestern,
   },
   {
     id: 27,
     Region: 'Sudbury',
     code: 3561,
     coordinate: { lat: 46.584537, lng: -81.3593029 },
+    Polygon: Sudbury,
   },
   {
     id: 28,
     Region: 'Thunder Bay',
     code: 3562,
     coordinate: { lat: 48.4027014, lng: -89.380607 },
+    Polygon: Thunderbay,
   },
   {
     id: 29,
     Region: 'Timiskaming',
     code: 3563,
     coordinate: { lat: 47.6685595, lng: -81.194061 },
+    Polygon: Timiskaming,
   },
   {
     id: 30,
     Region: 'Toronto',
     code: 3595,
     coordinate: { lat: 43.7184038, lng: -79.5181405 },
+    Polygon: toronto,
   },
   {
     id: 31,
     Region: 'Waterloo	',
     code: 3565,
     coordinate: { lat: 43.4823217, lng: -80.61675895 },
+    Polygon: Waterloo,
   },
   {
     id: 32,
     Region: 'Wellington Dufferin Guelph',
     code: 3566,
-    coordinate: { lat: 43.5345361, lng: -80.3102076 },
+    coordinate: { lat: 43.5345361, lng: -80.4102076 },
+    Polygon: Guelph,
   },
   {
     id: 33,
     Region: 'Windsor-Essex',
     code: 3568,
     coordinate: { lat: 42.2951067, lng: -83.0729216 },
+    Polygon: Windsor,
   },
   {
     id: 34,
     Region: 'York',
     code: 3570,
-    coordinate: { lat: 43.7123884, lng: -79.5470694 },
+    coordinate: { lat: 43.89973098887549, lng: -79.43899133899629 },
+    Polygon: York,
   },
 ];
